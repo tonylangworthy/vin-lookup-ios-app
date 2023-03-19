cec8d0968a57a9739524e38040282259a6023ec7
Tony Langworthy <langworthytech@gmail.com>
2023-03-19 07:46:08 -0500

    Added exclusions to gitignore

--- Pods/Target Support Files/Cause-Logic-Effect/Cause-Logic-Effect-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Cause_Logic_Effect : NSObject
-@end
-@implementation PodsDummy_Cause_Logic_Effect
-@end
--- Pods/Target Support Files/Pods-New VIN LookupTests/Pods-New VIN LookupTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_New_VIN_LookupTests : NSObject
-@end
-@implementation PodsDummy_Pods_New_VIN_LookupTests
-@end
